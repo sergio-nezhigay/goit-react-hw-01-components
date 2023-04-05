a4a5065cc9b1fb8413ba0b99978ca87559ceecc2
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-05 12:05:55 +0300

    1st task done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import user from "../../user.json";
+import { Profile } from "../Profile/Profile";
+
+export const App = () => {
+  return (     <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101'
+    }}
+  >
+     <Profile
+              username={user.username}
+              tag={user.tag}
+              location={user.location}
+              avatar={user.avatar}
+              stats={user.stats}
+            />
+  </div>
+           
+  );
+};
